686d74758d6d38f8028d7f7be6d59697cf58c662
Lucas Goedde <lgoedde@purdue.edu>
2016-02-06 20:55:11 -0500

    Maybe half way done

--- hw4/ece404_hw04_goedde.py
+++ hw4/ece404_hw04_goedde.py
@@ -1,4 +1,5 @@
 from BitVector import *
+import numpy as np
 
 AES_modulus = BitVector(bitstring='100011011')
 
@@ -36,7 +37,47 @@ def get_encryption_key():
 
 def aes_encrypt(input_file, encrypted_file):
     subtable, invsubtable = genTables()
+    bv = BitVector(filename=input_file)
+
+    while(bv.more_to_read):
+        bitvec = bv.read_bits_from_file(128)
+
+        if len(bitvec) != 128:
+            pad = 128 - len(bitvec)
+            bitvec.pad_from_left(pad)
+
+        sarray = [[0 for x in range(4)] for x in range(4)]
+
+        for i in range(4):
+            for j in range(4):
+                byte = bitvec[32*i+8*j:32*i+8*(j+1)]
+                sarray[j][i] = sub_bytes(byte, subtable)
+
+        shift_rows(sarray)
+        break
+
+def sub_bytes(byte, subtable):
+
+    row = byte.permute([0,1,2,3]).int_val()
+    col = byte.permute([4,5,6,7]).int_val()
+    #print(row,col)
+
+    #print(subtable[row*16+col])
+    return subtable[row*16+col]
+
+def shift_rows(sarray):
+    for i in range(4):
+        sarray[i] = list(np.roll(sarray[i], -i))
+
+def mix_columns(sarray):
+    pass
 
 
-    
+
+
+
+
+if __name__ == "__main__":
+    aes_encrypt("plaintext.txt", "encrypted.txt")
+
 
